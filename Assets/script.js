0bf55315f9bee5cc1cca396144c19f81fd6d0dfb
naywilkins512 <nathanwilkins01@gmail.com>
2020-08-24 22:40:29 -0500

    adds syntax for buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,89 @@
-$("#searchedCityBtn").on("click", function(event) {
 
+
+
+
+$("#searchedCityBtn").on("click", function(event) {
 
     event.preventDefault();
 
-    // Here we grab the text from the input box
     var city = $("#searchedCityInput").val();
-
-    // Here we construct our URL
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=78e6c921cefb9a49851564e781798bb6"
+    let queryURL2 = "  " + city + "&appid=78e6c921cefb9a49851564e781798bb6" //add api site to beginning of this line
     
-let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=78e6c921cefb9a49851564e781798bb6"
-let queryURL2 = " " + city + "&appid=78e6c921cefb9a49851564e781798bb6"
-
 
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-    .then(function(response) {
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+
        
-      $("#cityDisplay").text(city);
+      $("#cityDisplay").text(response.name);
       let tempF = Math.round((response.main.temp - 273.15) * 1.80 + 32);
       $("#tempDisplay").text("Temperature: " + tempF);
       $("#humidityDisplay").text("Humidity: " + response.main.humidity);
       $("#windDisplay").text("Wind: " + response.main.humidity);
-      
+
    
+})
 })
 
 
-$.ajax({
-    url: queryURL2,
-    method: "GET"
-  })
-  .then(function(response) {
- //   $("uvDisplay").text("UV: " + response.main.uv)
-});
+$("button"),this.on("click", function(event) {
 
-  })
+    event.preventDefault();
+
+    var city2 = $("button").this.val();
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city2 + "&appid=78e6c921cefb9a49851564e781798bb6"
+    // let queryURL2 = "  " + city + "&appid=78e6c921cefb9a49851564e781798bb6" //add api site to beginning of this line
+    
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+
+       
+      $("#cityDisplay").text(response.name);
+      let tempF = Math.round((response.main.temp - 273.15) * 1.80 + 32);
+      $("#tempDisplay").text("Temperature: " + tempF);
+      $("#humidityDisplay").text("Humidity: " + response.main.humidity);
+      $("#windDisplay").text("Wind: " + response.main.humidity);
+
+   
+})
+})
+
+// $("#austin").on("click", function(event) {
+//     event.preventDefault();
+//     var city = $("#austin").val();
+// let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=78e6c921cefb9a49851564e781798bb6"
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+//   .then(function(response) {
+     
+//     $("#cityDisplay").text(response.name);
+//     let tempF = Math.round((response.main.temp - 273.15) * 1.80 + 32);
+//     $("#tempDisplay").text("Temperature: " + tempF);
+//     $("#humidityDisplay").text("Humidity: " + response.main.humidity);
+//     $("#windDisplay").text("Wind: " + response.main.humidity);
+
+ 
+// })
+
+// })
+
+
+// $.ajax({
+//     url: queryURL2,
+//     method: "GET"
+//   })
+//   .then(function(response) {
+//  //   $("uvDisplay").text("UV: " + response.main.uv)
+// });
+
+//   })
