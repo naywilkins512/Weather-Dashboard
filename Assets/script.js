e25cbfe3a128c271d4bf7b98f73d8f9f95169f37
naywilkins512 <nathanwilkins01@gmail.com>
2020-08-27 14:24:13 -0500

    adds side bar functionality

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 
 //array to hold local storage of cities
 
-let cityArr = []
+let cityArr = localStorage.getItem("cityArr") || []
 
 init()
 
@@ -82,7 +82,7 @@ $("#searchedCityBtn").on("click", function (event) {
 
                     }
 
-                    let sidebutton = $("<button>")
+                    let sidebutton = $("<button>.")
                     sidebutton.addClass("sidebar list-group-item list-group-item-action")
                     sidebutton.text(city)
                     $(".sidebarbuttons").append(sidebutton)
@@ -103,10 +103,20 @@ function init() {
 
     // If cities were retrieved from localStorage, update the cities array to it
     if (storedCities !== null) {
-        cityArr = storedCities;
+        cityArr = storedCities
+    } else return;
+    for (let i = 0; i < storedCities.length; i++) {
+
+        let sidebutton2 = $("<button>")
+        sidebutton2.addClass("sidebar list-group-item list-group-item-action")
+        sidebutton2.text(JSON.stringify(storedCities[i].city))
+        $(".sidebarbuttons").append(sidebutton2)
+
+
     }
-    $(".sidebarbuttons").append(storedCities)
-    
+
+
+
 }
 
 
@@ -116,101 +126,92 @@ function init() {
 //figure out why it keeps appending.look into html method. why is html only adding one?     COMPLETE
 // make weather icon appear in main window. look above for jquery appending.                 COMPLETE
 // save search bar values to local storage                                                   COMPLETE
-// make sidebar value text pull from local storage and appear in sidebar
+// make sidebar value text pull from local storage and appear in sidebar                    COMPLETE
 //make sidebar values clickable in a way that would run the function
 // make buttons individual columns
 
 
-// $(".sidebar").each(function () {
+$(".sidebar").each(function () {
 
-//     $(this).on("click", function (event) {
-//         event.preventDefault()
+    $(this).on("click", function (event) {
+        event.preventDefault()
+        var city3 = this.val()
+        let currentWeatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city3 + "&appid=78e6c921cefb9a49851564e781798bb6"
 
-//         var city = $("#searchedCityInput").val();
-//         let currentWeatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=78e6c921cefb9a49851564e781798bb6"
+        // current weather API for name and Coords
 
-//         // current weather API for name and Coords
+        $.ajax({
+            url: currentWeatherQueryURL,
+            method: "GET"
+        })
+            .then(function (currentWeatherResponse) {
 
-//         $.ajax({
-//             url: currentWeatherQueryURL,
-//             method: "GET"
-//         })
-//             .then(function (currentWeatherResponse) {
+                console.log(currentWeatherResponse)
 
-//             }
-//   };
+                //one call API
 
-// $(".sidebar").each(function () {
-
-//         $(this).on("click", function (event) {
-//             event.preventDefault()
-
-//     var sidecity = $(this).val();
-//     let sideWeatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + sidecity + "&appid=78e6c921cefb9a49851564e781798bb6"
-
-//     // current weather API for name and Coords
-
-//     $.ajax({
-//         url: sideWeatherQueryURL,
-//         method: "GET"
-//     })
-//         .then(function (sideWeatherResponse) {
-
-//             console.log(sideWeatherResponse)
-
-//             //one call API
-
-//             let oneCallQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + sideWeatherResponse.coord.lat + "&lon=" + sideWeatherResponse.coord.lon +
-//                 "&exclude=minutely,hourly&units=imperial&appid=78e6c921cefb9a49851564e781798bb6"
+                let oneCallQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentWeatherResponse.coord.lat + "&lon=" + currentWeatherResponse.coord.lon +
+                    "&exclude=minutely,hourly&units=imperial&appid=78e6c921cefb9a49851564e781798bb6"
 
 
-//             $.ajax({
-//                 url: oneCallQueryURL,
-//                 method: "GET"
-//             })
-//                 .then(function (response) {
+                $.ajax({
+                    url: oneCallQueryURL,
+                    method: "GET"
+                })
+                    .then(function (response) {
 
-//                     //main display
+                        //main display
 
-//                     $("#cityDisplay").text(currentWeatherResponse.name);
-//                     $("#tempDisplay").text("Temperature: " + Math.round(response.current.temp));
-//                     $("#iconDisplay").attr("src", "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png")
-//                     $("#humidityDisplay").text("Humidity: " + response.current.humidity);
-//                     $("#windDisplay").text("Wind: " + Math.round(response.current.wind_speed));
-//                     $("#uvDisplay").text("UV Index: " + Math.round(response.current.uvi));
+                        $("#cityDisplay").text(currentWeatherResponse.name);
+                        $("#tempDisplay").text("Temperature: " + Math.round(response.current.temp));
+                        $("#iconDisplay").attr("src", "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png")
+                        $("#humidityDisplay").text("Humidity: " + response.current.humidity);
+                        $("#windDisplay").text("Wind: " + Math.round(response.current.wind_speed));
+                        $("#uvDisplay").text("UV Index: " + Math.round(response.current.uvi));
 
-//                     // uv index color indicator
+                        // uv index color indicator
 
-//                     if (response.current.uvi <= 6) {
-//                         $("#uvDisplay").css("background-color", "lightgreen")
-//                     } else if (response.current.uvi <= 10) {
-//                         $("#uvDisplay").css("background-color", "orange")
-//                     } else if (response.current.uvi >= 11) {
-//                         ($("#uvDisplay").css("background-color", "red"))
-//                     };
+                        if (response.current.uvi <= 6) {
+                            $("#uvDisplay").css("background-color", "lightgreen")
+                        } else if (response.current.uvi <= 10) {
+                            $("#uvDisplay").css("background-color", "orange")
+                        } else if (response.current.uvi >= 11) {
+                            ($("#uvDisplay").css("background-color", "red"))
+                        };
 
-//                     // 5 day forecast loop
-//                     $("#5dayForecast").empty()
+                        // 5 day forecast loop
+                        $("#5dayForecast").empty()
 
-//                     for (let i = 1; i < 6; i++) {
-//                         let forecastDiv = $("<div>");
-//                         forecastDiv.addClass("col-sm-2 border mx-3 Forecastdiv");
-//                         forecastDiv.text(dayArray[new Date(response.daily[i].dt * 1000).getDay()]);
-//                         let br = $("<br>");
-//                         let br2 = $("<br>");
-//                         let list = $("<div></div>")
-//                         let templi = $("<p></p>").text(Math.round(response.daily[i].temp.max))
-//                         let li2 = $("<p></p>").text(response.daily[i].weather[0].description)
-//                         let li3 = $("<img></img>")
-//                         li3.attr("src", "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png")
+                        for (let i = 1; i < 6; i++) {
+                            let forecastDiv = $("<div>");
+                            forecastDiv.addClass("col-sm-2 border mx-3 Forecastdiv");
+                            forecastDiv.text(dayArray[new Date(response.daily[i].dt * 1000).getDay()]);
+                            let br = $("<br>");
+                            let br2 = $("<br>");
+                            let list = $("<div></div>")
+                            let templi = $("<p></p>").text(Math.round(response.daily[i].temp.max))
+                            let li2 = $("<p></p>").text(response.daily[i].weather[0].description)
+                            let li3 = $("<img></img>")
+                            li3.attr("src", "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png")
 
 
-//                         list.append(templi, li3, li2)
-//                         forecastDiv.append(br, br2, list)
-//                         $("#5dayForecast").append(forecastDiv)
+                            list.append(templi, li3, li2)
+                            forecastDiv.append(br, br2, list)
+                            $("#5dayForecast").append(forecastDiv)
 
-//                     }
+                        }
 
-//                 })
-//         })
-// });
+                        let sidebutton = $("<button>")
+                        sidebutton.addClass("sidebar list-group-item list-group-item-action")
+                        sidebutton.text(city)
+                        $(".sidebarbuttons").append(sidebutton)
+                        cityArr.unshift({ city });
+                        localStorage.setItem("cityArr", JSON.stringify(cityArr));
+
+                        console.log(cityArr)
+
+                    })
+
+            })
+    })
+})
